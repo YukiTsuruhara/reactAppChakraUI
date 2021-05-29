d095723f091f66c3112e77520b8a46fb7deefa5b
YukiTsuruhara <y.tsuruhara@leihauoli.com>
2021-05-29 19:38:50 +0900

    hedda-

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import {ChakraProvider , Button} from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
+import theme from "./theme/theme";
+import { Router } from "./router/Router";
 
 const App = () => {
     return(
-        <ChakraProvider>
-            <div>
-                <Button colorScheme="blue">こんにちわ</Button>
-            </div>
+        <ChakraProvider theme={theme}>
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
         </ChakraProvider>
     );
 };
